3df5f5ef0dd26468ce961135663d14465acbc824
Michael <michaelbrockus@gmail.com>
2020-03-11 16:23:01 -0700

    revert back

--- mesonui/repository/mesonapi.py
+++ mesonui/repository/mesonapi.py
@@ -10,7 +10,6 @@ from ..mesonuilib.utilitylib import MesonUiException
 from .datascanner import MesonScriptReader
 from .datareader import MesonBuilddirReader
 from .dataloader import MesonBuilddirLoader
-from os.path import join as join_paths
 from pathlib import Path
 import os
 
@@ -26,7 +25,7 @@ class MesonAPI:
 
     def get_object(self, group: str = None, extract_method: str = 'script', use_fallback: bool = False) -> any:
         if extract_method == 'reader':
-            if use_fallback is False and os.path(join_paths(str(self._builddir), 'build.ninja')).exists():
+            if use_fallback is False and Path().joinpath(str(self._builddir), 'build.ninja').exists():
                 return self._builddir_reader.extract_from(group=group)
             elif use_fallback is True or Path().joinpath(str(self._sourcedir), 'meson.build').exists():
                 return self._script_scanner.extract_from(group=group)
@@ -34,7 +33,7 @@ class MesonAPI:
                 return None
 
         elif extract_method == 'loader':
-            if use_fallback is False and os.path(join_paths(str(self._builddir), 'build.ninja')).exists():
+            if use_fallback is False and Path().joinpath(str(self._builddir), 'build.ninja').exists():
                 return self._builddir_loader.extract_from(group=group)
             elif use_fallback is True or Path().joinpath(str(self._sourcedir), 'meson.build').exists():
                 return self._script_scanner.extract_from(group=group)
