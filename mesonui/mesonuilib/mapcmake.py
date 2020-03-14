73ca306064964fd1062282f6073fcc3c27c083ba
Michael <michaelbrockus@gmail.com>
2020-03-13 23:33:06 -0700

    move options maps into mapmeson.py

--- mesonui/mesonuilib/mapcmake.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-#
-# author : Michael Brockus.  
-# contact: <mailto:michaelbrockus@gmail.com>
-# license: Apache 2.0 :http://www.apache.org/licenses/LICENSE-2.0
-#
-# copyright 2020 The Meson-UI development team
-#
