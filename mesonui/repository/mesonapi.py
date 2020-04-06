04c7e36345c5233fb66c1ff70df4d5ca332104bf
Michael <michaelbrockus@gmail.com>
2020-04-05 21:58:12 -0700

    add logging in MesonAPI

--- mesonui/repository/mesonapi.py
+++ mesonui/repository/mesonapi.py
@@ -12,22 +12,26 @@ from .datareader import MesonBuilddirReader
 from .dataloader import MesonBuilddirLoader
 from pathlib import Path
 from os.path import join as join_paths
+import logging
 
 
 class MesonAPI:
-    def __init__(self, sourcedir: Path = None, builddir: Path = None):
+    def __init__(self, sourcedir: Path = Path().cwd(), builddir: Path = join_paths(Path().cwd(), 'builddir')):
         super().__init__()
         self._sourcedir: Path = sourcedir
         self._builddir: Path = builddir
         self._builddir_reader = MesonBuilddirReader(self._builddir)
         self._builddir_loader = MesonBuilddirLoader(self._builddir)
         self._script_scanner = MesonScriptReader(self._sourcedir)
+        logging.info(f'source: sourcedir={self._sourcedir}')
+    # end of method
 
     def get_object(self, group: str = None, extract_method: str = 'script', use_fallback: bool = False) -> any:
+        logging.info(f'protocol settings: use_fallback={use_fallback}, group={group}, extract={extract_method}')
         if extract_method == 'reader':
             if use_fallback is False and Path(self._builddir).exists():
                 return self._builddir_reader.extract_from(group=group)
-            elif use_fallback is True or Path(str(join_paths(self._sourcedir, 'meson.build'))).exists():
+            elif use_fallback is True or Path(join_paths(self._sourcedir, 'meson.build')).exists():
                 return self._script_scanner.extract_from(group=group)
             else:
                 return None
@@ -35,7 +39,7 @@ class MesonAPI:
         elif extract_method == 'loader':
             if use_fallback is False and Path(self._builddir).exists():
                 return self._builddir_loader.extract_from(group=group)
-            elif use_fallback is True or Path(str(join_paths(self._sourcedir, 'meson.build'))).exists():
+            elif use_fallback is True or Path(join_paths(self._sourcedir, 'meson.build')).exists():
                 return self._script_scanner.extract_from(group=group)
             else:
                 return None
