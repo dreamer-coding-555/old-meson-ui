abc45cfc2ef5579c8eedaa0e9dcae644a348d1d8
Michael <michaelbrockus@gmail.com>
2020-04-11 08:25:43 -0700

    rm targets info class

--- mesonui/mesonuilib/mesonapi/projectinfo.py
+++ mesonui/mesonuilib/mesonapi/projectinfo.py
@@ -29,14 +29,3 @@ class ProjectInfo:
         self.descriptive_name: str = re.sub(r'[^a-z0-9]', '_', self._project_info['descriptive_name'])
         self.subprojects: list = self._project_info['subprojects']
         self.number_of_subprojects: list = len(self._project_info['subprojects'])
-
-
-class TargetsInfo:
-    def __init__(self, meson_api: MesonAPI):
-        self.targetsinfo: any = meson_api.get_object(group='targets', extract_method='loader')
-        self.name = self._targets_info[0]['name']
-        self.type = self._targets_info[0]['type']
-        self.id = self._targets_info[0]['id']
-        self.language = self._targets_info[0]['target_sources'][0]['language']
-        self.compiler = self._targets_info[0]['target_sources'][0]['compiler']
-        self.parameters = self._targets_info[0]['target_sources'][0]['parameters']
